7070232b0b6b8e295bbed5d7fd09210ea9eae71f
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-09-10 14:05:54 -0700

    al[habet game in upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase then uppercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
