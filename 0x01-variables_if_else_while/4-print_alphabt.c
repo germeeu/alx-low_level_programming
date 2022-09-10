a63dcf01f06a3ae0d9e52dcda4e81f3eaa682915
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-09-10 14:14:00 -0700

    alphabet soup that payoff

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
