0cd6d89d3d056cb527881fc90fc1dea5cdcfd770
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-09-13 15:42:04 -0700

    alphabetic function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "a;phabet.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char leta = 'a';
+
+	while (leta <= 'z')
+	{
+		_putchar(leta);
+		leta++;
+	}
+	_putchar('\n');
+}
