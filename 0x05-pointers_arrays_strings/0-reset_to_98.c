24d46ea7d3235711723c679d1a98c6682676cd8a
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-09-19 06:54:22 -0700

    update of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @m: integer pointer to a variable to be updated
+ *
+ * Return: nothing
+ **/
+
+void reset_to_98(int *m)
+{
+		*m = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset theb value of a pointer to 98
+ *@m: the integer pointer variable to reset
+ * 
+ * Return: nothing
+ **/
+void reset_to_98(int *m)
+{
+	*m = 98;
+}
