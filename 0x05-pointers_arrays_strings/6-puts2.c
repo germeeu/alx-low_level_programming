1c6346a5449c15cb411cda402f874ed9d0d71c25
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-09-19 09:24:35 -0700

    print of a given string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int n = 0;
 
-	while (i >= 0)
+	while (str[n] != '\0')
 	{
-		if (str[i] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++,
+		{
+			_putchar(str[n]);
+		}
+		n++;
 	}
+	_putchar('\n');
 }
