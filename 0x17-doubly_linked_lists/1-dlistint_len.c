1af787dae0b0f3d9f21f215d39a47762def443a8
Muhammed Jamiu Saka <sakasim_jay@yahoo.com>
2022-12-08 11:22:46 -0800

    updates

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	int count;
 
-	while (h)
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		nodes++;
+		count++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
